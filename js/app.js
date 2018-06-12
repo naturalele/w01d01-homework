3c8d601fd37a9df39135784a245bde93f0a843b4
Kathleen Turner <kate@Kathleens-MacBook-Pro.local>
2018-06-12 08:48:52 -0600

    6.11

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// Section 1
+// 1. Interpolation is putting coressponding values into placeholders. ex. x + b = A
+// Concatentation allows you to add strings together using + sign. ex. var = red "My " + red + " wagon"
+// 2. Dry allows for simplier non repetitive code
+
+// Section 2
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+  console.log(a < b);
+  console.log(c > d);
+  console.log('Name' === 'Name');
+  console.log(a + b === c);
+  console.log(a * a === d);
+  console.log(e === 'Keven');
+  console.log(48 !== '48');
+
+  // Section 3
+  //Infinite Loop I true
+  //Infinite Loop II false
+  //Reading Code 20
+
+  //Section 4
+
+  //Secrion 5
+  
